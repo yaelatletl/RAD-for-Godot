66ba2c1b22d6169352143a37ede681e1b1abd007
yaelatletl <yael.atletl@gmail.com>
2018-03-16 08:52:00 -0600

    Update Portal.gd

--- BaseGD/Portals/Portal.gd
+++ BaseGD/Portals/Portal.gd
@@ -6,7 +6,7 @@ var materialout
 func _ready():
 	var origin=$Renderer/Viewport/Camera.get_camera_transform().origin
 	materialin = get_node(Out).materialout
-	materialout = $MeshInstance.get_surface_material(0)
+	materialout = $Renderer/Viewport.get_texture()
 	#$MeshInstance.set_surface_material(0, materialin)
 	var far = $Renderer/Viewport/Camera.far
 	
